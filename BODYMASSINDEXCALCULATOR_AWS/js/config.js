2a287742ef1e2e220bf6933e61714a694a089392
NivedhaSugumaran <43202131+NivedhaSugumaran@users.noreply.github.com>
2021-02-14 15:43:10 +0530

    Add files via upload

---
+++ BODYMASSINDEXCALCULATOR_AWS/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+
+        userPoolId: 'ap-south-1_W192y8kq3', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-south-1', // e.g. us-east-2
+		clientId: '5dot2c4qermqjq1f0l5sfa43bu' //is this used anywhere?
+
+    },
+    
+};
+
